2b62ae42bcbfa92a6eb4eb9902a5d9f6bee2d840
Carlos <jupton@gannett.com>
2015-03-07 10:12:29 -0500

    Created a detail page about each victim.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import render_template
 app = Flask(__name__)
 
 
+
 def get_csv():
 	csv_path = "./static/la-riots-death.csv"
 	csv_file = open(csv_path, "rb")
@@ -12,11 +13,23 @@ def get_csv():
 	return csv_list
 
 
+
 @app.route("/")
 def index():
 	template = "index.html"
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
+
+
+
+@app.route("/<row_id>/")
+def detail(row_id):
+	template = "detail.html"
+	object_list = get_csv()
+	for obj in object_list:
+		if obj['id'] == row_id:
+			return render_template(template, object=obj)
+
 
 
 if __name__ == "__main__":
